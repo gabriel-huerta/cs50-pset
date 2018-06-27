48b5ba663534522770a3c281bbd198ebda70b7bb
Gabriel <huerta.gabriel1991@gmail.com>
2018-06-26 20:58:03 -0700

    Final Commit

--- vigenere.c
+++ vigenere.c
@@ -9,56 +9,96 @@ int main(int argc, string argv[])
     //get key from user
             if (argc == 2)
             {
+
+            //get the length of the key
                 string key = argv[1];
 
+                int k = strlen(key);
+            //check if key is alphabetic
+            for (int i = 0; i < k; i++)
+            {
+            if (!isalpha(argv[1][i]))
+            {
+                printf("Key must be alphabetic chars only\n");
 
-                for (int i = 0; i < strlen(key); i++)
+                return 1;
+            }
+            }
+
+                //get plaintext
+                string plaintext = get_string("plaintext: " );
+
+                //get length of string
+                int p = strlen(plaintext);
+
+                printf("ciphertext: ");
+
+                //encipher single letter
+
+
+                for (int i = 0, j = 0; i < p; i++, j++)
                 {
-                    if (isupper(key[i]))
+
+                //if alphabetic
+                 if (isalpha(plaintext[i]))
+                 {
+
+                    if (j % k == 0)
                     {
-                       int ascii = (int) key[i];
+                        j = 0;
+                    }
+                     //if uppercase letter
+                    if (isupper(plaintext[i]))
+                    {
 
-                       int add = ascii - 65;
+                        int asciip = (int) plaintext[i];
 
-                       printf("%i ", add);
+                        int asciik = (int) key[j];
+
+                        if (asciik >= 97)
+                        {
+                            asciik = asciik - 32;
+                        }
+
+                        //print ciphertext
+                        printf("%c", (char) (((asciip - 65) + (asciik - 65) ) % 26 ) + 65);
 
                     }
-                    else
+                    else if (islower(plaintext[i]))
                     {
-                        int ascii = (int) key[i];
+                        int asciip = (int) plaintext[i];
 
-                        int add = ascii - 97;
+                        int asciik = (int) key[j];
 
-                        printf("%i ", add);
+                        if (asciik >= 97)
+                        {
+                            asciik = asciik - 32;
+                        }
+
+                        //print ciphertext
+                        printf("%c", (char) (((asciip - 97) + (asciik - 65) ) % 26 ) + 97);
                     }
+
+                }
+                else
+                {
+                    j--;
+                    printf("%c", plaintext[i]);
                 }
 
-
-
+                }
 
             }
-            else
+                else
             {
-                printf("error\n");
+                printf("Error");
                 return 1;
-
             }
 
 
-
-
-    //check if key is alphabetic
-
-    //get length of string
-
-
-    //get plaintext
-
-    //encipher
-
-    //encipher single letter
-
-    //print ciphertext
+    printf("\n");
     return 0;
+
+
 
 }
